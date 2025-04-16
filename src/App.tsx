18d9a57bddda6f80e0cfd01017856ad2cd32c640
langliu <lang.liu@heartmed.com.cn>
2025-04-16 20:06:05 +0800

    feat: 添加 @tauri-apps/plugin-log 插件，更新了相关依赖，优化了下载功能，增加了获取系统用户名的功能，调整了应用界面以显示用户名。

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,82 @@
 import { useEffect, useState } from "react";
 import { Command } from "@tauri-apps/plugin-shell";
+import { invoke } from "@tauri-apps/api/core";
 import { Button, Form, Input, Select } from "antd";
 import { LazyStore } from "@tauri-apps/plugin-store";
+import { info } from "@tauri-apps/plugin-log";
 import "./App.css";
 
 function App() {
   const store = new LazyStore("settings.json");
   const [downloading, setDownloading] = useState(false);
   const [form] = Form.useForm();
+  const [username, setUsername] = useState("");
   const [stdout, setStdout] = useState("");
-
 
   const onFinish = async (values: any) => {
     await store.set("cookie", values.cookie);
     setStdout("");
     setDownloading(true);
-    Command.create("f2", [
+    info(`start download ${values.mode} ${values.url}`);
+
+    const command = Command.create("f2", [
       "dy",
       "-M",
       values.mode,
       "-u",
       values.url,
       "-p",
-      "~/Downloads",
+      `/Users/${username}/Downloads`,
       "--cookie",
       values.cookie,
-    ])
-      .execute()
-      .then((res) => {
-        setStdout(res.stdout);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        setDownloading(false);
-      });
+    ]);
+
+    command.stdout.on("data", (line) => {
+      console.log("stdout data", line);
+      info(`stdout data ${line}`);
+      setStdout((prev) => prev + line);
+    });
+
+    command.stderr.on("data", (line) => {
+      setStdout((prev) => prev + "Error: " + line + "\n");
+    });
+
+    command.on("close", () => {
+      setDownloading(false);
+    });
+
+    command.on("error", (error) => {
+      console.error("Command error:", error);
+      setDownloading(false);
+    });
+    command.spawn();
   };
+
+  function getUsername() {
+    invoke<string>("get_system_username").then((res) => {
+      console.log(res);
+      setUsername(res);
+    });
+  }
 
   useEffect(() => {
     store.get("cookie").then((res) => {
       form.setFieldValue("cookie", res);
     });
+    getUsername();
   }, []);
 
   return (
     <main className="p-4">
-      <h1 className="text-2xl font-bold text-center">F2下载</h1>
-      <Form form={form} labelCol={{ span: 3 }} onFinish={onFinish}>
+      <h1 className="text-2xl font-bold text-center">
+        Hello {username} F2下载
+      </h1>
+      <Form
+        form={form}
+        labelCol={{ span: 3 }}
+        onFinish={onFinish}
+        disabled={downloading}
+      >
         <Form.Item name={"cookie"} label="Cookie" rules={[{ required: true }]}>
           <Input.TextArea placeholder="请输入Cookie" rows={4} />
         </Form.Item>
@@ -78,7 +107,9 @@ function App() {
           </Button>
         </Form.Item>
       </Form>
-      <pre>{stdout}</pre>
+      <pre className="mt-4 p-4 bg-gray-100 rounded-lg overflow-auto max-h-96">
+        {stdout}
+      </pre>
     </main>
   );
 }
