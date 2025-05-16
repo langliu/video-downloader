3c08cd3fdb3494cd864ce7d9c8f07484122f294a
langliu <lang.liu@heartmed.com.cn>
2025-05-16 18:48:21 +0800

    feat: 通过接口调用支持下载

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use fix_path_env::fix;
 
 fn main() {
-    let _ = fix_path_env::fix();
+    let _ = fix();
     f2_desktop_lib::run()
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
-import { Command } from "@tauri-apps/plugin-shell";
+import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { Button, Form, Input, Select } from "antd";
+import { Button, Form, Input, Space } from "antd";
 import { LazyStore } from "@tauri-apps/plugin-store";
-import { info } from "@tauri-apps/plugin-log";
-// import { BaseDirectory, exists } from "@tauri-apps/plugin-fs";
+import { openPath } from "@tauri-apps/plugin-opener";
+import { open } from "@tauri-apps/plugin-dialog";
 import "./App.css";
 
 function App() {
@@ -13,60 +12,85 @@ function App() {
   const [form] = Form.useForm();
   const [username, setUsername] = useState("");
   const [stdout, setStdout] = useState("");
+  const [urls, setUrls] = useState<string[]>([]);
+  const [videoList, setVideoList] = useState<
+    {
+      cover: string;
+      desc: string;
+      music: string;
+      playAddr: string;
+    }[]
+  >([]);
 
-  const onFinish = async (values: any) => {
-    await store.set("cookie", values.cookie);
-    setStdout("");
-    setDownloading(true);
-    info(`start download ${values.mode} ${values.url}`);
-    // await exists(`/Users/${username}/Downloads`, {});
+  /**
+   * 解析视频地址，获取无水印视频信息
+   * @param url 视频地址
+   */
+  const parseUrl = async (url: string) => {
+    const formData = new URLSearchParams();
+    formData.append("token", "uuic-qackd-fga-test");
+    formData.append("link", url);
 
-    const command = Command.create("f2", [
-      "dy",
-      "-M",
-      values.mode,
-      "-u",
-      values.url,
-      "-p",
-      `/Users/${username}/Downloads`,
-      "--cookie",
-      values.cookie,
-    ]);
-
-    command.stdout.on("data", (line) => {
-      console.log("stdout data", line);
-      info(`stdout data ${line}`);
-      setStdout((prev) => prev + line);
+    const res = await fetch("https://proxy.layzz.cn/lyz/platAnalyse/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formData,
     });
-
-    command.stderr.on("data", (line) => {
-      setStdout((prev) => prev + "Error: " + line + "\n");
-    });
-
-    command.on("close", () => {
-      setDownloading(false);
-    });
-
-    command.on("error", (error) => {
-      console.error("Command error:", error);
-      setDownloading(false);
-    });
-    command.execute();
+    const json = await res.json();
+    console.log("res", json);
+    if (json.code === "0001") {
+      setVideoList((prev) => [...prev, json.data]);
+    }
   };
 
-  function getUsername() {
-    invoke<string>("get_system_username").then((res) => {
-      console.log(res);
-      setUsername(res);
+  async function downloadUrl(url: string, fileName: string) {
+    const dir = await open({
+      multiple: false,
+      directory: true,
     });
+    if (dir) {
+      const result = await invoke("download_video", {
+        url,
+        savePath: dir,
+        fileName,
+      });
+      console.log("res", result);
+      
+    }
+    console.log("dir", dir);
   }
 
-  useEffect(() => {
-    store.get("cookie").then((res) => {
-      form.setFieldValue("cookie", res);
+  async function batchDownload() {
+    const dir = await open({
+      multiple: false,
+      directory: true,
     });
-    getUsername();
-  }, []);
+    if (dir) {
+      for (const video of videoList) {
+        await invoke("download_video", {
+        url:video.playAddr,
+        savePath: dir,
+        fileName:video.desc+'.mp4',
+      });
+      }
+      openPath(dir);
+    }
+  }
+
+  const onFinish = async (values: any) => {
+    const { urls } = values;
+    const urlList = urls
+      .split(/\n|,|，/)
+      .map((url: string) => url.trim())
+      .filter((url: string) => url);
+    console.log(urlList);
+    setUrls(urlList);
+    for (const url of urlList) {
+      await parseUrl(url);
+    }
+  };
 
   return (
     <main className="p-4">
@@ -79,34 +103,30 @@ function App() {
         onFinish={onFinish}
         disabled={downloading}
       >
-        <Form.Item name={"cookie"} label="Cookie" rules={[{ required: true }]}>
-          <Input.TextArea placeholder="请输入Cookie" rows={4} />
-        </Form.Item>
-        <Form.Item name="mode" label="下载模式" rules={[{ required: true }]}>
-          <Select placeholder="请选择URL对应的下载模式" allowClear>
-            <Select.Option value="one">单个作品</Select.Option>
-            <Select.Option value="post">主页作品</Select.Option>
-            <Select.Option value="like">点赞作品</Select.Option>
-            <Select.Option value="collection">收藏作品</Select.Option>
-            <Select.Option value="collects">收藏夹作品</Select.Option>
-            <Select.Option value="music">收藏音乐</Select.Option>
-            <Select.Option value="mix">合集</Select.Option>
-            <Select.Option value="live">直播</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name={"url"}
-          label="链接"
-          rules={[
-            { required: true, type: "url", message: "请输入正确的URL地址" },
-          ]}
-        >
-          <Input placeholder="请输入链接" />
+        <Form.Item name={"urls"} label="视频地址" rules={[{ required: true }]}>
+          <Input.TextArea
+            placeholder="请输入视频地址，以换行或者逗号分隔"
+            rows={4}
+          />
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit" loading={downloading}>
-            F2下载
-          </Button>
+          <Space>
+            <Button type="primary" htmlType="submit" loading={downloading}>
+              解析视频地址
+            </Button>
+            {videoList.length > 0 && (
+              <Button
+                type="primary"
+                htmlType="button"
+                loading={downloading}
+                onClick={() => {
+                  batchDownload();                  
+                }}
+              >
+                下载视频
+              </Button>
+            )}
+          </Space>
         </Form.Item>
       </Form>
       <pre className="mt-4 p-4 bg-gray-100 rounded-lg overflow-auto max-h-96">
